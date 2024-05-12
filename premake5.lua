848468b59149add4a0d6175a03725969a00c818e
babaliaris <babaliaris.nikos@gmail.com>
2024-05-12 05:02:30 +0300

    Init

---
+++ premake5.lua
@@ -0,0 +1,15 @@
+workspace "LearnSuperbible6"
+    configurations { "debug", "dist", "release"}
+    platforms "x64"
+    startproject "Sandbox"
+
+
+-- ||||||||||||||||||||Include Projects|||||||||||||||||||| --
+group "external"
+	include "external/superbible/external/superbible6/extern/glfw-2.7.6"
+    include "external/superbible/external/superbible6"
+group ""
+
+
+include "projects/Sandbox"
+-- ||||||||||||||||||||Include Projects|||||||||||||||||||| --
---
+++ projects/Sandbox/premake5.lua
@@ -0,0 +1,55 @@
+project "Sandbox"
+    targetname "sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "%{_WORKING_DIR}/builds/%{cfg.shortname}/"
+    objdir "%{_WORKING_DIR}/obj/%{prj.name}_%{cfg.shortname}/"
+
+    files {
+        "src/**.cpp",
+        "src/**.c",
+        "src/**.h",
+        "src/**.hpp"
+    }
+
+    includedirs {
+        "%{_WORKING_DIR}/external/superbible/external/superbible6/include",
+        "%{_WORKING_DIR}/external/superbible/external/superbible6/extern/glfw-2.7.6/include",
+    }
+
+    links {
+        "Superbible6",
+        "GLFW"
+    }
+
+    -- All configurations for LINUX.
+    filter "system:linux"
+        links {"GL", "X11", "pthread", "m",
+            "Xrandr", "Xi", "ldl"
+        }
+        
+        
+    -- All configurations for WINDOWS.
+    filter "system:windows"
+        kind "WindowedApp"
+        links {
+        "opengl32",
+        "winmm",
+        "gdi32"
+        }
+
+
+    --All Platforms.
+    filter{}
+    
+    filter "configurations:debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:dist"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:release"
+        runtime "Release"
+        optimize "on"
